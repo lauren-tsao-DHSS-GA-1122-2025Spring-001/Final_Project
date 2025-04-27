962a53055bc713e3a5a686c9b377129ceb406066
Lauren Tsao <et2676@nyu.edu>
2025-04-27 18:24:40 -0400

    updated code

--- script.js
+++ script.js
@@ -1,52 +1,78 @@
-let oneGIF = document.querySelectorAll('#one-GIF');
-let oneText = document.querySelector('#one-text');
+let oneGIF = document.querySelectorAll("#one-GIF");
+let oneText = document.querySelector("#one-text");
 
 oneGIF.forEach((gifContainer) => { 
-  gifContainer.addEventListener('mouseover', () => {
-    oneText.style.color = 'darkCyan'; 
+  gifContainer.addEventListener("mouseover", () => {
+    oneText.style.color = "darkCyan"; 
   });
 
-  gifContainer.addEventListener('mouseout', () => {
-    oneText.style.color = ''; 
+  gifContainer.addEventListener("mouseout", () => {
+    oneText.style.color = ""; 
   });
 });
 
-let twoGIF = document.querySelectorAll('#two-GIF');
-let twoText = document.querySelector('#two-text');
+let twoGIF = document.querySelectorAll("#two-GIF");
+let twoText = document.querySelector("#two-text");
 
 twoGIF.forEach((gifContainer) => { 
-  gifContainer.addEventListener('mouseover', () => {
-    twoText.style.color = 'coral'; 
+  gifContainer.addEventListener("mouseover", () => {
+    twoText.style.color = "orangeRed"; 
   });
 
-  gifContainer.addEventListener('mouseout', () => {
-    twoText.style.color = ''; 
+  gifContainer.addEventListener("mouseout", () => {
+    twoText.style.color = ""; 
   });
 });
 
-let threeGIF = document.querySelectorAll('#three-GIF');
-let threeText = document.querySelector('#three-text');
+let threeGIF = document.querySelectorAll("#three-GIF");
+let threeText = document.querySelector("#three-text");
 
 threeGIF.forEach((gifContainer) => { 
-  gifContainer.addEventListener('mouseover', () => {
-    threeText.style.color = 'darkOrchid'; 
+  gifContainer.addEventListener("mouseover", () => {
+    threeText.style.color = "darkOrchid"; 
   });
 
-  gifContainer.addEventListener('mouseout', () => {
-    threeText.style.color = ''; 
+  gifContainer.addEventListener("mouseout", () => {
+    threeText.style.color = ""; 
   });
 });
 
-let fourGIF = document.querySelectorAll('#four-GIF');
-let fourText = document.querySelector('#four-text');
+let fourGIF = document.querySelectorAll("#four-GIF");
+let fourText = document.querySelector("#four-text");
 
 fourGIF.forEach((gifContainer) => { 
-  gifContainer.addEventListener('mouseover', () => {
-    fourText.style.color = 'paleVioletRed'; 
+  gifContainer.addEventListener("mouseover", () => {
+    fourText.style.color = "blue"; 
   });
 
-  gifContainer.addEventListener('mouseout', () => {
-    fourText.style.color = ''; 
+  gifContainer.addEventListener("mouseout", () => {
+    fourText.style.color = ""; 
+  });
+});
+
+let fiveGIF = document.querySelectorAll("#five-GIF");
+let fiveText = document.querySelector("#five-text");
+
+fiveGIF.forEach((gifContainer) => { 
+  gifContainer.addEventListener("mouseover", () => {
+    fiveText.style.color = "#025a02"; 
+  });
+
+  gifContainer.addEventListener("mouseout", () => {
+    fiveText.style.color = ""; 
+  });
+});
+
+let sixGIF = document.querySelectorAll("#six-GIF");
+let sixText = document.querySelector("#six-text");
+
+sixGIF.forEach((gifContainer) => { 
+  gifContainer.addEventListener("mouseover", () => {
+    sixText.style.color = "#0047bc"; 
+  });
+
+  gifContainer.addEventListener("mouseout", () => {
+    sixText.style.color = ""; 
   });
 });
 
